29b3f4a1b008ee3ce0d419417f47704c0ca711bb
erenasena <erenasena13@gmail.com>
2020-05-11 18:17:46 +0200

    initial commit

---
+++ sensus/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sensus',
+      version='0.1',
+      description='A library for building a simple neural network.',
+      url='https://github.com/Programming-The-Next-Step/Neural_network.git',
+      author='Eren Asena',
+      author_email='eren.asena@student.uva.nl',
+      license='MIT',
+      packages=['sensus'],
+      zip_safe=False)
